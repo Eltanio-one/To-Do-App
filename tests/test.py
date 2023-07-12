799b02423c01bef48812ca03a9a51f2a10a11e37
Eltanio-one <haverd08@gmail.com>
2023-07-12 14:53:56 +0100

    unittest started

---
+++ tests/test.py
@@ -0,0 +1,11 @@
+import app
+import unittest
+
+
+class TestCase(unittest.TestCase):
+    def setUp(self):
+        app.app.testing = True
+        self.app = app.app.test_client()
+
+    def test_home(self):
+        result = self.app.get("/")
